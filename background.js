a736d4061938751cdca49c41ce003a01bbbe9e8f
Rishabh Jain <rishabhjain@Rishabhs-MacBook-Pro.local>
2018-11-03 13:12:49 -0400

    added warnings count storage on tab level

--- background.js
+++ background.js
@@ -3,83 +3,96 @@ var ips = ["216.58.194.164", "172.217.3.100", "192.229.173.207"];
 var ipset = new Set(ips);
 var count = 0;
 
-(function() {
-  const tabStorage = {};
-  const networkFilters = {
-      urls: [
-          "<all_urls>"
-      ]
-  };
+(function () {
+    const tabStorage = {};
+    const networkFilters = {
+        urls: [
+            "<all_urls>"
+        ]
+    };
 
-  
 
-  chrome.webRequest.onBeforeRequest.addListener((details) => {
-      const { tabId, requestId } = details;
-      if (!tabStorage.hasOwnProperty(tabId)) {
-          return;
-      }
+    chrome.tabs.onCreated.addListener(function (tabId, changeInfo, tab) {
+        
+    });
 
-      tabStorage[tabId].requests[requestId] = {
-          requestId: requestId,
-          url: details.url,
-          startTime: details.timeStamp,
-          status: 'pending'
-      };
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        var warnCountTemp = 0;
+        if(tabStorage[tabId].warning.url!=tab.url) {
+            tabStorage[tabId].warning.url = tab.url;
+            tabStorage[tabId].warning.count = 0;
+        }
 
-  }, networkFilters);
-  
+    });
 
-  chrome.webRequest.onCompleted.addListener((details) => {
-      const { tabId, requestId } = details;
-      if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
-          return;
-      }
+    chrome.webRequest.onBeforeRequest.addListener((details) => {
+        const { tabId, requestId } = details;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            return;
+        }
 
-      const request = tabStorage[tabId].requests[requestId];
-      console.log("---------start");
-      console.log(details);
-      console.log("---------end");
-      if(ipset.has(details.ip)) {
-        count+=1;
-      }
-      Object.assign(request, {
-          endTime: details.timeStamp,
-          requestDuration: details.timeStamp - request.startTime,
-          status: 'complete'
-      });
-      console.log(count);
-  }, networkFilters);
+        tabStorage[tabId].requests[requestId] = {
+            requestId: requestId,
+            url: details.url,
+            startTime: details.timeStamp,
+            status: 'pending'
+        };
+    }, networkFilters);
 
-  
-  chrome.webRequest.onErrorOccurred.addListener((details)=> {
-      const { tabId, requestId } = details;
-      if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
-          return;
-      }
 
-      const request = tabStorage[tabId].requests[requestId];
-      Object.assign(request, {
-          endTime: details.timeStamp,           
-          status: 'error',
-      });
-      console.log(tabStorage[tabId].requests[requestId]);
-  }, networkFilters);
+    chrome.webRequest.onCompleted.addListener((details) => {
+        const { tabId, requestId } = details;
+        if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+            return;
+        }
 
-  chrome.tabs.onActivated.addListener((tab) => {
-      const tabId = tab ? tab.tabId : chrome.tabs.TAB_ID_NONE;
-      if (!tabStorage.hasOwnProperty(tabId)) {
-          tabStorage[tabId] = {
-              id: tabId,
-              requests: {},
-              registerTime: new Date().getTime()
-          };
-      }
-  });
-  chrome.tabs.onRemoved.addListener((tab) => {
-      const tabId = tab.tabId;
-      if (!tabStorage.hasOwnProperty(tabId)) {
-          return;
-      }
-      tabStorage[tabId] = null;
-  });
+        const request = tabStorage[tabId].requests[requestId];
+        
+        if (ipset.has(details.ip)) {
+            tabStorage[tabId].warning.count += 1;
+        }
+        Object.assign(request, {
+            endTime: details.timeStamp,
+            requestDuration: details.timeStamp - request.startTime,
+            status: 'complete'
+        });
+        console.log(tabStorage[tabId].warning.count);
+    }, networkFilters);
+
+
+    chrome.webRequest.onErrorOccurred.addListener((details) => {
+        const { tabId, requestId } = details;
+        if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+            return;
+        }
+
+        const request = tabStorage[tabId].requests[requestId];
+        Object.assign(request, {
+            endTime: details.timeStamp,
+            status: 'error',
+        });
+        console.log(tabStorage[tabId].requests[requestId]);
+    }, networkFilters);
+
+    chrome.tabs.onActivated.addListener((tab) => {
+        const tabId = tab ? tab.tabId : chrome.tabs.TAB_ID_NONE;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            tabStorage[tabId] = {
+                id: tabId,
+                requests: {},
+                warning: {
+                    url: "",
+                    count: 0
+                },
+                registerTime: new Date().getTime()
+            };
+        }
+    });
+    chrome.tabs.onRemoved.addListener((tab) => {
+        const tabId = tab.tabId;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            return;
+        }
+        tabStorage[tabId] = null;
+    });
 }());
