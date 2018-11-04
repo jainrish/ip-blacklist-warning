3c939acb9661f91fd78e88c935ee20b9871a2acb
Rishabh Jain <rishabhjain@Rishabhs-MacBook-Pro.local>
2018-11-04 11:16:49 -0500

    show blacklisted ips on popup

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+//https://www.w3schools.com/sql/default.asp - sample
 (function () {
     'use strict';
     var utils;
@@ -6,16 +7,36 @@
     function initPopupPage() {
         initWhiteListBtnStatus();
         function initWhiteListBtnStatus() {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                if(!tabs) {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                if (!tabs) {
                     return;
                 }
                 let ctab = tabs[0];
                 document.getElementById('blacklistedIPCount').innerText = background.tabStorage[ctab.id].warning.count;
+                let prop,
+                    trEl,
+                    tdEl,
+                    tableEl = document.getElementById('blockedIPs');
+
+                background.tabStorage[ctab.id].warning.ipList.forEach(element => {
+                    trEl = document.createElement('tr');
+                    tdEl = document.createElement('td');
+                    tdEl.innerText = element;
+                    trEl.appendChild(tdEl);
+                    tableEl.appendChild(trEl);
+                });
+                setWlistStatus(true);
                 console.log(ctab.id);
             });
         }
 
+    }
+
+    function setWlistStatus(status) {
+        document.getElementById('addWlist').style.display = (status === true) ? 'none' : '';
+        document.getElementById('removeWlist').style.display = (status === true) ? '' : 'none';
+        document.getElementById('hideWl').style.display = (status === true) ? '' : '';
+        document.getElementById('hidePs').style.display = (status === true) ? '' : '';
     }
     initPopupPage();
 
